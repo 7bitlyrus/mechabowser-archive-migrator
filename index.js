84c914def5319e5252221682e0f7ae23088152bb
Ian Murray <theianm2002@ymail.com>
2020-05-28 23:28:04 -0400

    Header test and some prepwork for insert

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
-
 const config = require("./config.json");
 
 const dbClient = new MongoClient(config.mongo, {useUnifiedTopology: true});
+
+// CONSTANTS
+const HEADER_OLDSTYLE = /^# Message archive for \"#.{10,}\" \(\d+\) in guild ".+" \((\d{10,})\)\n{1,2}# Format:\n\[date \+ time] Member ID\/Message ID\/Username - Message content\n----------------$/;
+const HEADER_NEWSTYLE = /^# Message archive for guild ".+" \((\d{10,})\)\nIncluded channels: .+\n# Format:\n\[date \+ time] Member ID\/Message ID\/Channel\/Username - Message content\n----------------$/;
 
 (async function() {
     // --- Connect to database and set exposed collections
@@ -43,10 +46,31 @@ const dbClient = new MongoClient(config.mongo, {useUnifiedTopology: true});
     
     for await(const doc of archiveCursor) {
         try {
-            console.log(`Converting ${doc._id}...`)
+            const id = doc._id;
+            const body = doc.body;
+            const timestamp = doc.timestamp;
+            // console.log(`Converting ${id}...`);
+
             
-            // TODO: Magic
-            console.log(doc)
+            const lines = body.split('\n');
+            
+            // Extract Header
+            var header = "";
+            for(i in lines) {
+                const line = lines[i];
+                header += line;
+                if(line === "----------------") break;
+                else header += '\n';
+            };
+
+            // Test Header
+            if(header.match(HEADER_OLDSTYLE)) console.log(`👌 ${id} matches old-style header`);
+            else if(header.match(HEADER_NEWSTYLE)) console.log(`👌 ${id} matches new-style header`);
+            else console.log(`⚠️ ${id} does not match any known header type`);
+
+            // TODO: DO MAGIC
+
+            // console.log(doc);
         } catch (err) {
             console.log("Failed to convert document:", err.stack);
             process.exit(1);
